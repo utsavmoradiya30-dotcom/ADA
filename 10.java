e09b1a984ab6855dabd05684bfbfc08ce263baa0
utsavmoradiya30-dotcom <utsavmoradiya30@gmail.com>
2026-05-01 20:45:14 +0530

    Create 10.java

---
+++ 10.java
@@ -0,0 +1,27 @@
+public class Knapsack {
+
+    public static int knapsack(int W, int[] wt, int[] val, int n) {
+        int[][] dp = new int[n + 1][W + 1];
+
+        for (int i = 0; i <= n; i++) {
+            for (int w = 0; w <= W; w++) {
+                if (i == 0 || w == 0)
+                    dp[i][w] = 0;
+                else if (wt[i - 1] <= w)
+                    dp[i][w] = Math.max(val[i - 1] + dp[i - 1][w - wt[i - 1]], dp[i - 1][w]);
+                else
+                    dp[i][w] = dp[i - 1][w];
+            }
+        }
+
+        return dp[n][W];
+    }
+
+    public static void main(String[] args) {
+        int[] wt = {1, 3, 4, 5};
+        int[] val = {1, 4, 5, 7};
+        int W = 7;
+
+        System.out.println("Max value: " + knapsack(W, wt, val, wt.length));
+    }
+}
