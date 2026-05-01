8cac8ee9fcb22ffabd40410f2efae07507c7d039
utsavmoradiya30-dotcom <utsavmoradiya30@gmail.com>
2026-05-01 20:46:49 +0530

    Create 13.java

---
+++ 13.java
@@ -0,0 +1,33 @@
+public class SCS {
+
+    public static int scsLength(String X, String Y) {
+        int m = X.length();
+        int n = Y.length();
+
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 0; i <= m; i++)
+            dp[i][0] = i;
+
+        for (int j = 0; j <= n; j++)
+            dp[0][j] = j;
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (X.charAt(i - 1) == Y.charAt(j - 1))
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+
+        return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+        String X = "AGGTAB";
+        String Y = "GXTXAYB";
+
+        System.out.println("SCS length: " + scsLength(X, Y));
+    }
+}
